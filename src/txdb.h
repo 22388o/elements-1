3d5f911500d835ee80bb2a9d899f70832d201a6c
Jonathan Gonzales <rojikkushisutemu@gmail.com>
2022-11-20 11:13:23 -0800

    txdb: pegin outpoint spending check removed

--- src/txdb.h
+++ src/txdb.h
@@ -61,8 +61,6 @@ public:
     std::vector<uint256> GetHeadBlocks() const override;
     bool BatchWrite(CCoinsMap &mapCoins, const uint256 &hashBlock) override;
     std::unique_ptr<CCoinsViewCursor> Cursor() const override;
-    // ELEMENTS:
-    bool IsPeginSpent(const std::pair<uint256, COutPoint> &outpoint) const override;
 
     //! Attempt to update from an older database format. Returns whether an error occurred.
     bool Upgrade();
