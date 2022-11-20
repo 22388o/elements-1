4540900365165d4099cbe8b03ffd60c2a2f121c2
Jonathan Gonzales <rojikkushisutemu@gmail.com>
2022-11-20 12:13:33 -0800

    MempoolCache: Pegin outpoints removed from oncomming Connecting Blocks, temporarily removing assetPairs for right now

--- src/validation.h
+++ src/validation.h
@@ -729,8 +729,9 @@ public:
 
     // Block (dis)connection on a given view:
     DisconnectResult DisconnectBlock(const CBlock& block, const CBlockIndex* pindex, CCoinsViewCache& view);
+    // std::set<std::pair<unit256> assetPair removed
     bool ConnectBlock(const CBlock& block, BlockValidationState& state, CBlockIndex* pindex,
-                      CCoinsViewCache& view, std::set<std::pair<uint256, COutPoint>>* setPeginsSpent, bool fJustCheck = false) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+                      CCoinsViewCache& view, bool fJustCheck = false) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
 
     // Apply the effects of a block disconnection on the UTXO set.
     bool DisconnectTip(BlockValidationState& state, DisconnectedBlockTransactions* disconnectpool) EXCLUSIVE_LOCKS_REQUIRED(cs_main, m_mempool->cs);
